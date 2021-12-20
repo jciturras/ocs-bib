00a3c840524a69cf47e6b19f4d39764ad1e63235
Julio César Iturra Sanhueza <julioiturrasanhueza@gmail.com>
2021-12-20 20:40:11 -0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Dec 17 18:12:13 2021
+
+@author: Julio
+"""
+
+import pathlib
+
+from flask import Flask
+from flask_babel import Babel
+from flask_bootstrap import Bootstrap
+
+from kerko import blueprint as kerko_blueprint
+from kerko.composer import Composer
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'ciencia123'  # Replace this value.
+app.config['KERKO_ZOTERO_API_KEY'] = 'Luoum41MvIMNqSLTitax7URA'  # Replace this value.
+app.config['KERKO_ZOTERO_LIBRARY_ID'] = '2635555'  # Replace this value.
+app.config['KERKO_ZOTERO_LIBRARY_TYPE'] = 'group'  # Replace this value if necessary.
+app.config['KERKO_DATA_DIR'] = str(pathlib.Path(__file__).parent / 'data' / 'kerko')
+app.config['KERKO_FACET_COLLAPSING'] = 'True'
+app.config['KERKO_COMPOSER'] = Composer()
+
+babel = Babel(app)
+bootstrap = Bootstrap(app)
+
+app.register_blueprint(kerko_blueprint, url_prefix='/bibliography')
