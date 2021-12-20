7b42d77fdef7afad911162fca2ffb0642346a1f9
jciturras <jciturra@uc.cl>
2021-12-20 20:58:32 -0300

    app2

--- app2.py
+++ app2.py
@@ -1,14 +1,8 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Fri Dec 17 18:12:13 2021
-
-@author: Julio
-"""
-
 from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/")
-def index():
-    return "Hello this is the new version!"
+
+@app.route('/')
+def hello():
+    return 'Hello, World!'
