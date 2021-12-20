8b473439f16ab85e3a4dde4226fcf6f6578c2c67
jciturras <jciturra@uc.cl>
2021-12-20 20:51:13 -0300

    Update app.py

--- app.py
+++ app.py
@@ -6,12 +6,13 @@ Created on Fri Dec 17 18:12:13 2021
 """
 
 import pathlib
+import os
 
 from flask import Flask
 from flask_babel import Babel
 from flask_bootstrap import Bootstrap
 
-from kerko import blueprint as kerko_blueprint
+from kerko import blueprint  as kerko_blueprint
 from kerko.composer import Composer
 
 app = Flask(__name__)
@@ -22,8 +23,8 @@ app.config['KERKO_ZOTERO_LIBRARY_TYPE'] = 'group'  # Replace this value if neces
 app.config['KERKO_DATA_DIR'] = str(pathlib.Path(__file__).parent / 'data' / 'kerko')
 app.config['KERKO_FACET_COLLAPSING'] = 'True'
 app.config['KERKO_COMPOSER'] = Composer()
-
+port = os.environ.get("PORT", 5000)
 babel = Babel(app)
 bootstrap = Bootstrap(app)
-
+app.run(debug=False, host="0.0.0.0", port=port)
 app.register_blueprint(kerko_blueprint, url_prefix='/bibliography')
