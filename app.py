2048d0d58219e3b33b99a930f80ba0ae3d78fad3
jciturras <jciturra@uc.cl>
2021-12-20 21:15:18 -0300

    kerko1

--- app.py
+++ app.py
@@ -1,51 +1,31 @@
-# app.py
-from flask import Flask, request, jsonify
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Dec 17 18:12:13 2021
+
+@author: Julio
+"""
+
+import pathlib
+import os
+
+from flask import Flask
+from flask_babel import Babel
+from flask_bootstrap import Bootstrap
+
+from kerko import blueprint  as kerko_blueprint
+from kerko.composer import Composer
+
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'ciencia123'  # Replace this value.
+app.config['KERKO_ZOTERO_API_KEY'] = 'Luoum41MvIMNqSLTitax7URA'  # Replace this value.
+app.config['KERKO_ZOTERO_LIBRARY_ID'] = '2635555'  # Replace this value.
+app.config['KERKO_ZOTERO_LIBRARY_TYPE'] = 'group'  # Replace this value if necessary.
+app.config['KERKO_DATA_DIR'] = str(pathlib.Path(__file__).parent / 'data' / 'kerko')
+app.config['KERKO_FACET_COLLAPSING'] = 'True'
+app.config['KERKO_COMPOSER'] = Composer()
 
-@app.route('/getmsg/', methods=['GET'])
-def respond():
-    # Retrieve the name from url parameter
-    name = request.args.get("name", None)
-
-    # For debugging
-    print(f"got name {name}")
-
-    response = {}
-
-    # Check if user sent a name at all
-    if not name:
-        response["ERROR"] = "no name found, please send a name."
-    # Check if the user entered a number not a name
-    elif str(name).isdigit():
-        response["ERROR"] = "name can't be numeric."
-    # Now the user entered a valid name
-    else:
-        response["MESSAGE"] = f"Welcome {name} to our awesome platform!!"
-
-    # Return the response in json format
-    return jsonify(response)
-
-@app.route('/post/', methods=['POST'])
-def post_something():
-    param = request.form.get('name')
-    print(param)
-    # You can add the test cases you made in the previous function, but in our case here you are just testing the POST functionality
-    if param:
-        return jsonify({
-            "Message": f"Welcome {name} to our awesome platform!!",
-            # Add this option to distinct the POST request
-            "METHOD" : "POST"
-        })
-    else:
-        return jsonify({
-            "ERROR": "no name found, please send a name."
-        })
-
-# A welcome message to test our server
-@app.route('/')
-def index():
-    return "<h1>Welcome to our server !!</h1>"
-
-if __name__ == '__main__':
-    # Threaded option to enable multiple instances for multiple user access support
-    app.run(threaded=True, port=5000)
+babel = Babel(app)
+bootstrap = Bootstrap(app)
+port = os.environ.get("PORT", 5000)
+app.run(debug=False, host="0.0.0.0", port=port)
+app.register_blueprint(kerko_blueprint, url_prefix='/bibliography')
